bd0be48381318c5e90fed3b9aa0a88f0a45f067d
Shadrackssien <shadrack6960@gmail.com>
2024-03-23 19:56:34 +0000

    Line spacing

--- events-data-hooks/meals-list-context-api/src/components/MealsList.js
+++ events-data-hooks/meals-list-context-api/src/components/MealsList.js
@@ -6,9 +6,13 @@ const MealsList = () => {
     return (
         <div>
             <h1>Meals List Using Context API</h1>
-            {meals.map((meal, index) => (
-                <h2 key={index}>{meal}</h2>
-            ))}
+
+            {
+            meals.map( (meal, index) => (
+                <h2 key={index}> {meal} </h2>
+            )
+            )
+            }
         </div>
     )
 }
