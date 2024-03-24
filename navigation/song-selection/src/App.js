48089f4119a4c24814f6a91b7517f0765a70440d
Shadrackssien <shadrack6960@gmail.com>
2024-03-24 20:28:02 +0000

    song selection

--- navigation/song-selection/src/App.js
+++ navigation/song-selection/src/App.js
@@ -1,20 +1,38 @@
-import ReactPlayer from "react-player";
-import './App.css';
+import React from "react"; 
+ 
+function App() { 
+ 
+  const bird1 = new Audio( 
+    "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3" 
+  ); 
+ 
+  const bird2 = new Audio( 
+    "https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3" 
+  ); 
+ 
+  function toggle1() { 
+    if (bird1.paused) { 
+      bird1.play(); 
+    } else { 
+      bird1.pause(); 
+    } 
+  }; 
 
-function App() {
-
-  const vidUrl = "https://www.youtube.com/watch?v=FZzchDH5S_U";
-
-  return (
-    <div className="App">
-      <h1>React Player example</h1>
-      <ReactPlayer
-        url={vidUrl}
-        playing={false}
-        volume={0.5} 
-      />
-    </div>
-  );
-}
-
+ 
+  function toggle2() { 
+    if (bird2.paused) { 
+      bird2.play(); 
+    } else { 
+      bird2.pause(); 
+    } 
+  }; 
+ 
+  return ( 
+    <div> 
+      <button onClick={toggle1}>Caspian Tern 1</button> 
+      <button onClick={toggle2}>Caspian Tern 2</button> 
+    </div> 
+  ); 
+} 
+ 
 export default App;
